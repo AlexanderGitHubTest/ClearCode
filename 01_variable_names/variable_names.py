bb37e75d000b623bd8479e0642e4f542188a183a
AlexanderGitHubTest <94744815+AlexanderGitHubTest@users.noreply.github.com>
2023-12-11 22:27:22 +0300

    Added a report on Variable Names

---
+++ 01_variable_names/variable_names.py
@@ -0,0 +1,39 @@
+"""
+Variable names.
+"""
+
+result - found_slot_id
+# id найденного слота
+
+result - hash_of_string
+# хэш строки
+
+c - character_from_string
+# символ из строки
+
+result - result_of_union_operation
+# результат операции union
+
+dict - processed_operations
+# словарь, содержащий обрабатываемые операции
+
+obj - character_from_expression
+# символ из выражения
+
+obj - character_from_first_stack
+# символ из первого стека
+
+list_of_db - accounts_receivable_files
+# список файлов с дебиторской задолженностью
+
+db_file - file_with_accounts_receivable
+# файл с дебиторской задолженностью
+
+balance - customer_balance_at_last_second_of_period
+# баланс клиента в последнюю секунду периода
+
+postponement - date_until_which_client_granted_deferral
+# дата, до которой клиенту предоставляется отсрочка
+
+status - status_of_phone_number_on_specified_date
+# статус телефонного номера в указанную дату
